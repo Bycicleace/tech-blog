a937ec604360e6480b7f12b4953c1f436ca20143
kvammee <elliott.kvamme@gmail.com>
2022-01-22 12:49:56 -0600

    Finished home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -40,7 +40,58 @@ router.get('/', (req, res) => {
 });
 
 // Render Login
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
 
 // Render Existing Post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'body',
+            'created_at',
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['body'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: "No post found with this id." });
+            return;
+        }
+        
+        const post = dbPostData.get({ plain: true });
+
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
 
 module.exports = router;
