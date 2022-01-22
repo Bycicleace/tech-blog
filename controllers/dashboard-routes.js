2a499ac699e1f771497976238d3cab2338172c3c
kvammee <elliott.kvamme@gmail.com>
2022-01-22 15:12:33 -0600

    Added initial dashboard route

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'body',
+            'created_at',
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['body'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/index');
 const homeRoutes = require('./home-routes');
+const dashRoutes = require('./dashboard-routes');
 
 router.use('/api',apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
