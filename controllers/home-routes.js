606b737094f0d321706465d5a5e6c47af1e53b71
kvammee <elliott.kvamme@gmail.com>
2022-01-22 12:36:06 -0600

    Updated home-routes and homepage to render correctly

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,7 +31,7 @@ router.get('/', (req, res) => {
         res.render('homepage', {
             posts,
             loggedIn: req.session.loggedIn
-        })
+        });
     })
     .catch(err => {
         console.log(err);
